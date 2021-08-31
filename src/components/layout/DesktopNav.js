98a12de0bc0df6270163885adab3749b555620b2
dandavisjs <dandavisjs@protonmail.com>
2021-08-30 18:59:56 -0700

    responsive nav

--- src/components/layout/DesktopNav.js
+++ src/components/layout/DesktopNav.js
@@ -1,25 +1,43 @@
-import {useRef} from 'react'
+import {useRef,useEffect} from 'react'
 import Navbar from'./Navbar'
 import {motion} from 'framer-motion'
+import { useMediaQuery } from 'react-responsive'
 
 export default function DesktopNav() {
+  useEffect(()=>{
+    isMobile && console.log('test');
+  },[])
   const date = new Date().getFullYear()
   // const checkActive 
+
   const menuRef = useRef()
-  
+  const isMobile = useMediaQuery({query:'(max-width: 768px)'})
   return (
     <div id='sidebar' >
       <span style={{fontWeight:'bold', fontSize:20, marginRight:25}}>DAVISjs</span>
       <Navbar />
-      <span className="clickable"><i className="nav-lines las la-bars" style={{fontSize:30}} onClick={()=>{
-        return menuRef.current.style.display = 'block'
-        }}></i></span>
+      <span className="clickable">
+        <i 
+        className="nav-lines las la-bars" 
+        style={{fontSize:30}} 
+        onClick={()=>{
+          return menuRef.current.style.display = 'block'
+        }}></i>
+      </span>
+      {isMobile &&
       <motion.div className="nav-slider" ref={menuRef}>
-      <Navbar />
-        <span className="clickable"><i class="las la-times" style={{fontSize:30}} onClick={()=>{
-        return menuRef.current.style.display = 'none'
-        }}></i></span>
+        <Navbar />
+        <span className="clickable">
+          <i 
+          class="las la-times" 
+          style={{fontSize:30}} 
+          onClick={()=>{
+            return menuRef.current.style.display = 'none'
+          }}
+          ></i>
+        </span>
       </motion.div>
+      }
     </div>
   )
 }
